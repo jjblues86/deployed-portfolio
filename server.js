71cf54172433b92a8b1cc49899d004ffea4aadb7
Jerome Joof <jeromejoof@yahoo.com>
2019-12-06 16:17:30 -0800

    initialize server

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+const express = require('express');
+
+//App
+const app = express();
+
+app.use(express.static('./public'));
+
+app.get('/jabinSmiles', function(req, res){
+  console.log('Jabin smiles bright like a diamond');
+  res.send({personality:'He is adorable',
+    gender: 'male',
+  });
+})
+
+
+app.listen(3000, function() {
+  // res.send('you made it!')
+  console.log(`app is up on PORT 3000`);
+});
+
+
+// app.listen(PORT, () => {
+//   console.log(`app is up on port : ${PORT}`);
+// })
